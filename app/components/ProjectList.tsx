a27f76be937e9185c0b8e21a980acbed7d0a20a9
Elif Gömleksiz <e.goemlek@gmail.com>
2023-04-19 15:21:51 +0200

    implement project filtering functionality to ProjectList component

--- app/components/ProjectList.tsx
+++ app/components/ProjectList.tsx
@@ -1,40 +1,38 @@
+"use client";
+
+import { useState } from "react";
 import { roboto_mono, roboto } from "../fonts";
 import Button from "./Button";
 import Emoji from "./Emoji";
 import Tag from "./Tag";
 import TaskSuggestion from "./TaskSuggestion";
 
-type todo = {
-  text: string;
-  created_at: string;
-  completed_at: string | undefined;
-  isCompleted: boolean;
-};
-
-type Project = {
-  id: number;
-  title: string;
-  description: string | undefined;
-  created_at: string;
-  completed_at: string | undefined;
-  notes: string | undefined;
-  tutorials: string | undefined;
-  materials: string[];
-  todos: todo[];
-  isCompleted: boolean;
-};
+import { TagLabel, Project } from "../types";
 
 type ProjectListProps = {
   projects: Project[];
 };
 
-const tags = [
-  { text: "all", symbol: "✨" },
-  { text: "in progress", symbol: "🔨", label: "hammer" },
-  { text: "done", symbol: "👏", label: "clapping hands" },
+type Tag = {
+  label: TagLabel;
+  emoji: string;
+  emojiLabel?: string;
+};
+
+const tags: Tag[] = [
+  { label: "all", emoji: "✨" },
+  { label: "in progress", emoji: "🔨", emojiLabel: "hammer" },
+  { label: "done", emoji: "👏", emojiLabel: "clapping hands" },
 ];
 
 export default function ProjectList({ projects }: ProjectListProps) {
+  const [filteredProjects, setFilteredProjects] = useState(projects);
+  const [selectedTag, setSelectedTag] = useState<TagLabel>("all");
+
+  const handleFilterChange = (projects: Project[], tagLabel: TagLabel) => {
+    setFilteredProjects(projects);
+    setSelectedTag(tagLabel);
+  };
   return (
     <>
       <div className=" mt-8 md:hidden">
@@ -55,33 +53,44 @@ export default function ProjectList({ projects }: ProjectListProps) {
               </div>
             </div>
 
-            <div className="flex flex-wrap gap-3 ">
-              {tags.map((tag) => (
-                <Tag key={tag.text}>
-                  {tag.text} <Emoji label={tag.label} symbol={tag.symbol} />
-                </Tag>
-              ))}
-            </div>
+            {filteredProjects && (
+              <div className="flex flex-wrap gap-3 ">
+                {tags.map((tag) => (
+                  <Tag
+                    key={tag.label}
+                    handleFilterChange={handleFilterChange}
+                    tagLabel={tag.label}
+                    projects={projects}
+                    isSelected={selectedTag === tag.label}
+                  >
+                    {tag.label}{" "}
+                    <Emoji label={tag.emojiLabel} symbol={tag.emoji} />
+                  </Tag>
+                ))}
+              </div>
+            )}
           </div>
 
-          <div className="mr-auto mt-10 flex max-w-2xl flex-wrap-reverse justify-center">
-            {projects.map((project, i) => {
-              return (
-                <div
-                  key={project.id}
-                  className={`group flex
+          {filteredProjects && (
+            <div className="mr-auto mt-10 flex max-w-2xl flex-wrap-reverse justify-center">
+              {filteredProjects.map((project, i) => {
+                return (
+                  <div
+                    key={project.id}
+                    className={`group flex
                        h-24 min-w-[140px] cursor-pointer
                    flex-col justify-center rounded-[10px] border border-lightblue bg-darkblue hover:bg-softgreen mobile:w-[calc(100%/2)] mobile:nth-[3n+3]:mx-10 sm:w-[calc(100%/3)] sm:nth-[5n+4]:ml-10 sm:nth-[5n+5]:mr-10   `}
-                >
-                  <p
-                    className={`${roboto_mono.variable} p-3 text-center font-mono text-fontblue group-hover:text-darkblue `}
                   >
-                    {project.title}
-                  </p>
-                </div>
-              );
-            })}
-          </div>
+                    <p
+                      className={`${roboto_mono.variable} p-3 text-center font-mono text-fontblue group-hover:text-darkblue `}
+                    >
+                      {project.title}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
         <div className="mobile:hidden sm:hidden md:block">
           <TaskSuggestion />
--- app/components/Tag.tsx
+++ app/components/Tag.tsx
@@ -1,16 +1,55 @@
 import { ReactNode } from "react";
 import { roboto_mono } from "../fonts";
+import { TagLabel, Project } from "../types";
+
+type HandleFilterChange = (projects: Project[], tagText: TagLabel) => void;
 
 type TagProps = {
   children: ReactNode;
+  handleFilterChange: HandleFilterChange;
+  tagLabel: TagLabel;
+  projects: Project[];
+  isSelected: boolean;
 };
 
-export default function Tag({ children }: TagProps) {
+const handleTagSelect = (
+  tagLabel: TagLabel,
+  projects: Project[],
+  handleFilterChange: HandleFilterChange
+) => {
+  let filteredProjects = projects;
+  if (tagLabel === "in progress") {
+    filteredProjects = projects.filter(
+      (project: Project) => project.isCompleted === false
+    );
+  } else if (tagLabel === "done") {
+    filteredProjects = projects.filter(
+      (project: Project) => project.isCompleted === true
+    );
+  }
+  return handleFilterChange(filteredProjects, tagLabel);
+};
+
+export default function Tag({
+  children,
+  tagLabel,
+  handleFilterChange,
+  projects,
+  isSelected,
+}: TagProps) {
+  const handleClick = () => {
+    handleTagSelect(tagLabel, projects, handleFilterChange);
+  };
   return (
-    <div
-      className={` inline-block cursor-pointer rounded-[5px] bg-lightblue px-3 py-1 ${roboto_mono.variable} font-mono text-sm text-darkblue hover:bg-softgreen`}
+    <button
+      className={` inline-block cursor-pointer rounded-[5px] ${
+        isSelected ? "bg-softgreen" : "bg-lightblue"
+      } px-3 py-1 ${
+        roboto_mono.variable
+      } font-mono text-sm text-darkblue hover:bg-softgreen`}
+      onClick={handleClick}
     >
       {children}
-    </div>
+    </button>
   );
 }
