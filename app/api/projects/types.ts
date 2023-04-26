f46b40011224b712d6982bf03d580e37b34b4801
Elif Gömleksiz <e.goemlek@gmail.com>
2023-04-26 18:48:50 +0200

    update projects data structure

--- app/api/projects/types.ts
+++ app/api/projects/types.ts
@@ -4,18 +4,19 @@ export type Project = {
   id: number;
   title: string;
   description: string | undefined;
-  created_at: string;
-  completed_at: string | undefined;
+  createdAt: string;
+  completedAt: string | undefined;
   notes: string | undefined;
   tutorials: string | undefined;
-  materials: string[];
+  materialsAtHand: string[];
+  missingMaterials: string[];
   todos: Todo[];
   isCompleted: boolean;
 };
 
 export type Todo = {
-  text: string;
-  created_at: string;
-  completed_at: string | undefined;
+  task: string;
+  createdAt: string;
+  completedAt?: string;
   isCompleted: boolean;
 };
