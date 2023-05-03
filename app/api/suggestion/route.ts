aa5fa497332e4a5e3f36b99d473d786490e1ad03
Elif Gömleksiz <e.goemlek@gmail.com>
2023-05-03 10:22:41 +0200

    undo generateProjectDataForPrompt function & add function to retrieve the most recently created and completed tasks

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,64 +1,108 @@
 import projects from "../projects/projects.json";
 import { NextResponse } from "next/server";
 import { Project } from "../projects/types";
+import { Todo } from "../projects/types";
 
-// type TaskForPrompt = {
-//   task: string;
-//   createdAt: string;
-//   completedAt?: string;
-// };
+/* 
+Project data string for GPT prompt: 
 
-// type ProjectDataForPrompt = {
-//   title: string;
-//   createdAt: string;
-//   materialsAtHand: string[];
-//   missingMaterials: string[];
-//   recentlyCompletedTasks: TaskForPrompt[];
-//   openTasks: TaskForPrompt[];
-// };
+Project: Arched Garden Shelf
+Materials at hand: drywall, oak shelves  
+Missing materials: plywood
+Completed tasks:
+- Buy jigsaw, completed on 2023-4-14
+- Research jigsaw type, completed on 2023-04-13
+Open tasks:
+- Take measurements for materials, created on 2023-04-11
+- Cut plywood, created on 2023-04-11
 
-const generateProjectDataForPrompt = (projects: Project[]) => {
-  if (projects.length === 0) return `No projects created yet`;
+Project: wood art
+Materials at hand: wood
+Missing materials: wood paint
+Completed tasks:
+- Decide on wood arrangement, completed on 2023-04-15
+Open tasks:
+- Cut wood, created on 2023-04-15
+- Paint wood cuts, created on 2023-04-15
+ */
 
-  const openProjects = projects.filter(
-    ({ isCompleted }) => isCompleted === false
-  );
-
-  const newProjectsArray = openProjects.map(
-    ({ title, createdAt, materialsAtHand, missingMaterials, todos }) => {
-      const openTasks = todos
-        .filter(({ isCompleted }) => isCompleted === false)
-        .map((openTodo) => {
-          const { isCompleted, completedAt, ...newOpenTodo } = openTodo;
-          return newOpenTodo;
-        });
-      const recentlyCompletedTasks = todos
-        .filter(({ isCompleted }) => isCompleted === true)
-        .map((todo) => {
-          const { isCompleted, ...newTodo } = todo;
-          return newTodo;
-        }) // sort functionality to take only tasks completed in the last 30 days? or take the newest completed tasks?
-        .sort((a, b) =>
-          a.completedAt && b.completedAt
-            ? new Date(a.completedAt).getTime() -
-              new Date(b.completedAt).getTime()
-            : 0
-        );
-      return {
-        [title]: {
-          createdAt,
-          materialsAtHand,
-          missingMaterials,
-          recentlyCompletedTasks,
-          openTasks,
-        },
-      };
-    }
-  );
-
-  return newProjectsArray;
+type ProjectWithTasks = {
+  title: string;
+  todos: Todo[];
 };
 
-type ProjectDataForPrompt = ReturnType<typeof generateProjectDataForPrompt>;
+type SingleTask = {
+  project: string;
+  taskDate: string;
+};
 
-console.log(generateProjectDataForPrompt(projects));
+const generateMostRecentTasks = (
+  tasks: ProjectWithTasks[],
+  taskStatus: "open" | "completed"
+) =>
+  tasks.reduce((accumulator: SingleTask[], project: ProjectWithTasks) => {
+    const recentlyUpdatedTasks = project.todos
+      .filter(({ isCompleted }) =>
+        taskStatus === "completed" ? isCompleted : !isCompleted
+      )
+      .map((todo) =>
+        taskStatus === "completed" ? todo.completedAt : todo.createdAt
+      );
+
+    recentlyUpdatedTasks.forEach((taskDate) => {
+      if (taskDate && accumulator.length === 0) {
+        accumulator.push({ project: project.title, taskDate });
+        return;
+      }
+
+      if (
+        taskDate &&
+        taskDate > accumulator.slice(-1)[0].taskDate &&
+        accumulator.length >= 3
+      )
+        return;
+
+      if (taskDate && accumulator.length <= 3) {
+        accumulator.push({
+          project: project.title,
+          taskDate,
+        });
+      }
+
+      if (
+        taskDate &&
+        taskDate <= accumulator.slice(-1)[0].taskDate &&
+        accumulator.length > 3
+      ) {
+        accumulator.pop();
+        accumulator.push({
+          project: project.title,
+          taskDate,
+        });
+      }
+      accumulator.sort((taskA: SingleTask, taskB: SingleTask) => {
+        const dateA = new Date(taskA.taskDate);
+        const dateB = new Date(taskB.taskDate);
+        return dateA.getTime() - dateB.getTime();
+      });
+    });
+
+    return accumulator;
+  }, []);
+
+const generateMostRecentProject = (projects: Project[]) => {
+  console.log("generateMostRecentProject called");
+
+  const tasks = projects
+    .filter(({ isCompleted }) => isCompleted === false)
+    .map(({ title, todos }) => {
+      return { title, todos };
+    });
+
+  const recentlyCompletedTasks = generateMostRecentTasks(tasks, "completed");
+  const recentlyCreatedTasks = generateMostRecentTasks(tasks, "open");
+
+  return [...recentlyCompletedTasks, ...recentlyCreatedTasks];
+};
+
+console.log(generateMostRecentProject(projects));
