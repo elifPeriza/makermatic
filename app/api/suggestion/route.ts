e97831cf8d48cfe80efc01c164364f33959e49c3
Elif Gömleksiz <e.goemlek@gmail.com>
2023-05-07 15:47:33 +0200

    Implement function to calculate recently most active project using generateMostRecentTasks function

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -27,82 +27,107 @@ Open tasks:
  */
 
 type ProjectWithTasks = {
-  title: string;
+  projectID: number;
   todos: Todo[];
 };
 
 type SingleTask = {
-  project: string;
+  projectID: number;
   taskDate: string;
 };
 
 const generateMostRecentTasks = (
-  tasks: ProjectWithTasks[],
+  projectsWithTasks: ProjectWithTasks[],
   taskStatus: "open" | "completed"
 ) =>
-  tasks.reduce((accumulator: SingleTask[], project: ProjectWithTasks) => {
-    const recentlyUpdatedTasks = project.todos
-      .filter(({ isCompleted }) =>
-        taskStatus === "completed" ? isCompleted : !isCompleted
-      )
-      .map((todo) =>
-        taskStatus === "completed" ? todo.completedAt : todo.createdAt
-      );
+  projectsWithTasks.reduce(
+    (accumulator: SingleTask[], project: ProjectWithTasks) => {
+      const taskDates = project.todos
+        .filter(({ isCompleted }) =>
+          taskStatus === "completed" ? isCompleted : !isCompleted
+        )
+        .map((todo) =>
+          taskStatus === "completed" ? todo.completedAt : todo.createdAt
+        );
 
-    recentlyUpdatedTasks.forEach((taskDate) => {
-      if (taskDate && accumulator.length === 0) {
-        accumulator.push({ project: project.title, taskDate });
-        return;
-      }
+      taskDates.forEach((taskDate) => {
+        if (taskDate && accumulator.length === 0) {
+          accumulator.push({ projectID: project.projectID, taskDate });
+          return;
+        }
 
-      if (
-        taskDate &&
-        taskDate > accumulator.slice(-1)[0].taskDate &&
-        accumulator.length >= 3
-      )
-        return;
+        if (
+          taskDate &&
+          taskDate > accumulator.slice(-1)[0].taskDate &&
+          accumulator.length >= 3
+        )
+          return;
 
-      if (taskDate && accumulator.length <= 3) {
-        accumulator.push({
-          project: project.title,
-          taskDate,
+        if (taskDate && accumulator.length <= 3) {
+          accumulator.push({
+            projectID: project.projectID,
+            taskDate,
+          });
+        }
+
+        if (
+          taskDate &&
+          taskDate <= accumulator.slice(-1)[0].taskDate &&
+          accumulator.length > 3
+        ) {
+          accumulator.pop();
+          accumulator.push({
+            projectID: project.projectID,
+            taskDate,
+          });
+        }
+        accumulator.sort((taskA: SingleTask, taskB: SingleTask) => {
+          const dateA = new Date(taskA.taskDate);
+          const dateB = new Date(taskB.taskDate);
+          return dateA.getTime() - dateB.getTime();
         });
-      }
-
-      if (
-        taskDate &&
-        taskDate <= accumulator.slice(-1)[0].taskDate &&
-        accumulator.length > 3
-      ) {
-        accumulator.pop();
-        accumulator.push({
-          project: project.title,
-          taskDate,
-        });
-      }
-      accumulator.sort((taskA: SingleTask, taskB: SingleTask) => {
-        const dateA = new Date(taskA.taskDate);
-        const dateB = new Date(taskB.taskDate);
-        return dateA.getTime() - dateB.getTime();
       });
-    });
 
-    return accumulator;
-  }, []);
+      return accumulator;
+    },
+    []
+  );
 
-const generateMostRecentProject = (projects: Project[]) => {
-  console.log("generateMostRecentProject called");
-
-  const tasks = projects
+const calculateMostRecentProject = (projects: Project[]) => {
+  const projectsWithTasks = projects
     .filter(({ isCompleted }) => isCompleted === false)
-    .map(({ title, todos }) => {
-      return { title, todos };
+    .map(({ id: projectID, todos }) => {
+      return { projectID, todos };
     });
 
-  const recentlyCompletedTasks = generateMostRecentTasks(tasks, "completed");
-  const recentlyCreatedTasks = generateMostRecentTasks(tasks, "open");
+  const recentlyCompletedTasks = generateMostRecentTasks(
+    projectsWithTasks,
+    "completed"
+  );
+  const recentlyCreatedTasks = generateMostRecentTasks(
+    projectsWithTasks,
+    "open"
+  );
 
-  return [...recentlyCompletedTasks, ...recentlyCreatedTasks];
+  const mostRecentTasks = [...recentlyCompletedTasks, ...recentlyCreatedTasks];
+
+  let taskCountPerProject: { [key: string]: number } = {};
+
+  mostRecentTasks.forEach((task) => {
+    taskCountPerProject[task.projectID] ??= 0;
+    taskCountPerProject[task.projectID]++;
+  });
+
+  const projectIDWithMostRecentTasks = parseInt(
+    Object.keys(taskCountPerProject).reduce((acc, curr) =>
+      taskCountPerProject[acc] >= taskCountPerProject[curr] ? acc : curr
+    )
+  );
+
+  const mostRecentProject = projects.find(
+    (project) => project.id === projectIDWithMostRecentTasks
+  );
+  return mostRecentProject;
 };
 
-console.log(generateMostRecentProject(projects));
+console.log(calculateMostRecentProject(projects));
