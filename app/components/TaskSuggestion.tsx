82378c6a4723aee5ef223bfc0acee52e94719be0
Elif Gömleksiz <e.goemlek@gmail.com>
2023-05-17 14:48:52 +0200

    Fetch and display task suggestion from API in frontend

--- app/components/TaskSuggestion.tsx
+++ app/components/TaskSuggestion.tsx
@@ -3,7 +3,18 @@ import Button from "./Button";
 import Emoji from "./Emoji";
 import HourGlass from "./HourGlass";
 
-export default function TaskSuggestion() {
+async function getTaskSuggestion() {
+  const response = await fetch("http://localhost:3000/api/suggestion", {
+    next: { revalidate: 60 * 60 * 24 },
+  });
+
+  const suggestion = await response.json();
+  return suggestion;
+}
+
+export default async function TaskSuggestion() {
+  const { taskSuggestion, estimatedTime } = await getTaskSuggestion();
+
   return (
     <div className=" flex min-w-[250px] max-w-sm flex-col gap-3 ">
       <h2
@@ -15,21 +26,24 @@ export default function TaskSuggestion() {
         <p
           className={`${roboto.variable} mr-3 max-w-xs font-sans text-base text-white`}
         >
-          Based on today's sunny weather, why not cut the plywood for your
-          Arched Garden Shelf project?
+          {taskSuggestion
+            ? taskSuggestion
+            : `Hello there! I'm your DIY Task Assistant and here to help you make the most out of your creative endeavors. Let's embark on this exciting journey together! Just share the status of your most active project, and I'll provide you with a daily task suggestion that will keep you moving forward. No matter the challenge, I'm here to encourage and support you every step of the way. `}
         </p>
         <Emoji label="sun" symbol="☀️" />
       </div>
       <div className="mt-4 flex flex-row justify-between">
         <Button variant="secondary">Go to task</Button>
-        <div className="flex flex-row gap-3">
-          <p
-            className={`${roboto.variable} max-w-xs self-center font-sans text-base text-white`}
-          >
-            25 min.
-          </p>
-          <HourGlass />
-        </div>
+        {estimatedTime && (
+          <div className="flex flex-row gap-3">
+            <p
+              className={`${roboto.variable} max-w-xs self-center font-sans text-base text-white`}
+            >
+              {estimatedTime}
+            </p>
+            <HourGlass />
+          </div>
+        )}
       </div>
     </div>
   );
