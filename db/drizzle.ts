54b18a3961283afd3ca59ea974f1f30c88419da5
elif <elifg07@gmail.com>
2023-07-13 12:26:22 +0200

    Update database schema

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,12 +1,11 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema";
 
 // Connection to the SQLite Database, creating a client object with the URL to the database file
 const client = createClient({
   url: "file:db/dev.db",
 });
-//console.log({ client });
 
 //// Creating a drizzle object from the client object which we need to work with the database
-export const db = drizzle(client);
-//console.log({ db });
+export const db = drizzle(client, { schema });
--- db/schema.ts
+++ db/schema.ts
@@ -1,23 +1,87 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-import { sql } from "drizzle-orm";
+import { InferModel, relations, sql } from "drizzle-orm";
+
+export const users = sqliteTable("users", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+});
+
+export type User = InferModel<typeof users, "select">;
+
+export const usersRelations = relations(users, ({ many }) => ({
+  projects: many(projects),
+}));
 
 export const projects = sqliteTable("projects", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id),
   name: text("name").notNull().default("new project"),
   description: text("description"),
-  createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
   completedAt: text("completed_at"),
   isCompleted: integer("is_completed").notNull().default(0),
   notes: text("notes"),
   colors: text("colors").notNull().default("87CEEB,FF7F50,FFD700"),
 });
 
+export type Project = InferModel<typeof projects, "select">;
+
+export const projectsRelations = relations(projects, ({ many, one }) => ({
+  tasks: many(tasks),
+  user: one(users, {
+    fields: [projects.userId],
+    references: [users.id],
+  }),
+}));
+
 export const tasks = sqliteTable("tasks", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   projectId: integer("project_id").references(() => projects.id),
   description: text("description").notNull().default("new task"),
   type: text("type").notNull().default("to-do"),
-  createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
   completedAt: text("completed_at"),
   isCompleted: integer("is_completed").notNull().default(0),
 });
+export type Tasks = InferModel<typeof tasks, "select">;
+
+export const tasksRelations = relations(tasks, ({ one }) => ({
+  project: one(projects, {
+    fields: [tasks.projectId],
+    references: [projects.id],
+  }),
+}));
+
+export const taskSuggestions = sqliteTable("tasksuggestions", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  content: text("content"),
+  userId: integer("user_id").references(() => users.id),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  taskId: integer("task_id").references(() => tasks.id),
+});
+
+export type TaskSuggestion = InferModel<typeof taskSuggestions, "select">;
+
+export const taskSuggestionsRelations = relations(
+  taskSuggestions,
+  ({ one }) => ({
+    user: one(users, {
+      fields: [taskSuggestions.userId],
+      references: [users.id],
+    }),
+    task: one(tasks, {
+      fields: [taskSuggestions.taskId],
+      references: [tasks.id],
+    }),
+  })
+);
