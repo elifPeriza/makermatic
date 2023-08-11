ece3eba14f9c99472b1c958ba21c1a62378e795c
elif <elifg07@gmail.com>
2023-08-11 22:30:11 +0200

    Simplify zod validation schema structure

--- app/validations/newProjectSchema.ts
+++ app/validations/newProjectSchema.ts
@@ -1,41 +1,4 @@
 import { z } from "zod";
-
-export const NewProjectInput = z.object({
-  name: z
-    .string()
-    .nonempty({
-      message: "Looks like you forgot to name your project.",
-    })
-    .min(3, {
-      message: "Your project name needs to be at least 3 characters long.",
-    })
-    .max(50, {
-      message: "Wow, that's a long name! Let's keep it under 50 characters.",
-    }),
-  description: z
-    .string()
-    .min(3, {
-      message:
-        "Please provide a more detailed description (at least 3 characters)",
-    })
-    .max(200, {
-      message:
-        "Your description is a bit too long. Let's keep it under 200 characters.",
-    })
-    .optional()
-    .or(z.literal("")),
-  notes: z
-    .string()
-    .max(1000, {
-      message:
-        "Your notes are a bit too long. Let's keep them under 500 characters.",
-    })
-    .optional(),
-});
-
-export type NewProjectInputType = z.infer<typeof NewProjectInput>;
-export type NewProjectSchema = typeof NewProjectInput;
-
 export const NewProjectInsert = z.object({
   userId: z.number().positive(),
   colorPaletteId: z.number().positive(),
@@ -70,6 +33,14 @@ export const NewProjectInsert = z.object({
     })
     .optional(),
 });
+
+export const NewProjectInput = NewProjectInsert.omit({
+  userId: true,
+  colorPaletteId: true,
+});
+
+export type NewProjectInputType = z.infer<typeof NewProjectInput>;
+export type NewProjectSchema = typeof NewProjectInput;
 
 export const ProjectUpdate = NewProjectInput.partial();
 export type ProjectUpdateType = z.infer<typeof ProjectUpdate>;
